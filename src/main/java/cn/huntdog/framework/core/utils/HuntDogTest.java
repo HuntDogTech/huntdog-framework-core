5de2e643a27538a0f75a0e01bd08a564d98f1238
JonDai <jondai@aliyun.com>
2016-07-06 14:45:44 +0800

    init core

---
+++ src/main/java/cn/huntdog/framework/core/utils/HuntDogTest.java
@@ -0,0 +1,11 @@
+package cn.huntdog.framework.core.utils;
+
+/**
+ * Created by JonDai on 2016/7/6.
+ */
+public class HuntDogTest {
+    public static String getString(){
+        return "Hello World!";
+    }
+
+}
